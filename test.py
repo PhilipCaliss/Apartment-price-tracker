5bfad427bf6f459d73e4c87564185d7b245efcb4
Philip Calissendorff <philip.calissendorff@hotmail.com>
2023-10-25 20:24:17 +0200

    more git ignore

--- test.py
+++ test.py
@@ -4,20 +4,34 @@ import requests
 BASE_URL = "https://www.hemnet.se/bostader?location_ids%5B%5D=925964&item_types%5B%5D=bostadsratt&page="
 headers = {'User-Agent': '*'}
 page_num = 1
+all_listings = []
 
 while True:
     url = BASE_URL + str(page_num)
     response = requests.get(url, headers=headers)
-    
+
     # If the server responds with 404, break out of the loop
     if response.status_code == 404:
         print(f"Reached end at page {page_num}. Server responded with 404.")
         break
     
     soup = BeautifulSoup(response.text, 'html.parser')
-    titles = soup.select("h2.listing-card__street-address.qa-listing-title")
-    
-    for title in titles:
-        print(f"[Response {response.status_code} - Page {page_num}] {title.text.strip()}")
+    listings = soup.select("div.listing-card__information-container")
+
+    print(f"Page {page_num} detected listings: {len(listings)}")
+
+    for listing in listings:
+        title_tag = listing.select_one("h2.listing-card__street-address.qa-listing-title")
+        
+        listing_id_tag = listing.parent.parent.get('id') if listing.parent.parent else None  # Fetch id from the grandparent
+        
+        if title_tag and listing_id_tag:
+            listing_data = {
+                'title': title_tag.text.strip(),
+                'listing_id': listing_id_tag.replace('listing_', '')  # Remove the 'listing_' prefix
+            }
+            all_listings.append(listing_data)
     
     page_num += 1
+
+print(all_listings)
--- test3.py
+++ test3.py
@@ -1,48 +1,84 @@
 from bs4 import BeautifulSoup
 import requests
+import pandas as pd
+
+
 
 BASE_URL = "https://www.hemnet.se/bostader?location_ids%5B%5D=925964&item_types%5B%5D=bostadsratt&page="
-headers = {'User-Agent': '*'} # This is to avoid getting blocked by the server, check www.hemnet.se/robots.txt
-page_num = 1
+LISTING_URL = "https://www.hemnet.se/bostad/"
+headers = {"User-Agent": "*"}
 
-
-
-response = requests.get(BASE_URL + str(page_num), headers=headers)
-soup = BeautifulSoup(response.text, 'html.parser')
-listings = soup.select("li.normal-results__hit")
-
-# Only inspecting the first listing for debugging
-listing = listings[0]
-
-# Extracting the details
-title_tag = listing.select_one("h2.listing-card__street-address.qa-listing-title")
-listing_id_attr = listing.attrs.get("data-tracking-data")
-if listing_id_attr:
-    listing_id = eval(listing_id_attr).get("listingId")
-else:
-    listing_id = None
 
 def clean_price(price):
-    return price.replace('\xa0', ' ')
+    return price.replace("\xa0", " ")
 
+def get_page_soup(page_num):
+    response = requests.get(f"{BASE_URL}{page_num}", headers=headers)
+    if response.status_code == 404:
+        return None
+    return BeautifulSoup(response.text, "html.parser")
 
-listed_price = clean_price(listing.select_one("div.listing-card__attributes-row div.listing-card__attribute--primary").text.strip()) if listing.select_one("div.listing-card__attributes-row div.listing-card__attribute--primary") else None
-apartment_size = listing.select_one("div.listing-card__attributes-row > div:nth-child(2)").text.strip() if listing.select_one("div.listing-card__attributes-row > div:nth-child(2)") else None
-num_rooms = listing.select_one("div.listing-card__attributes-row > div:nth-child(3)").text.strip() if listing.select_one("div.listing-card__attributes-row > div:nth-child(3)") else None
-monthly_price = clean_price(listing.select_one("div.listing-card__attributes-row + div.listing-card__attributes-row div.listing-card__attribute--fee").text.strip()) if listing.select_one("div.listing-card__attributes-row + div.listing-card__attributes-row div.listing-card__attribute--fee") else None
+def get_broker_info(listing_id):
+    url = f"{LISTING_URL}{listing_id}"
+    response = requests.get(url, headers=headers)
+    if response.status_code != 200:
+        return None
+    return BeautifulSoup(response.text, "html.parser")
 
-# Creating the dictionary
-apartment_data = {
-    listing_id: {
-        "Title": title_tag.text.strip() if title_tag else 'Not Found',
-        "Listed Price": listed_price,
-        "Apartment Size": apartment_size,
-        "Number of Rooms": num_rooms,
-        "Monthly Price": monthly_price
+def get_listing_details(listing):
+    def get_text(selector):
+        element = listing.select_one(selector)
+        return element.text.strip() if element else None
+
+    listing_id_attr = listing.attrs.get("data-tracking-data")
+    listing_id = eval(listing_id_attr).get("listingId") if listing_id_attr else None
+
+    return {
+        "Title": get_text("h2.listing-card__street-address.qa-listing-title"),
+        "Listed Price": clean_price(get_text("div.listing-card__attributes-row div.listing-card__attribute--primary")),
+        "Apartment Size": get_text("div.listing-card__attributes-row > div:nth-child(2)"),
+        "Number of Rooms": get_text("div.listing-card__attributes-row > div:nth-child(3)"),
+        "Monthly Price": clean_price(get_text("div.listing-card__attributes-row + div.listing-card__attributes-row div.listing-card__attribute--fee"))
     }
-}
 
-#TODO: Refactor above to iterate through all the results and pages.
-#TODO: To fetch brokers, go to www.hemnet.se/bostad/{listing_id} and extract the broker name from the page
+def get_broker_company_name(listing_id):
+    broker_info = get_broker_info(listing_id)
+    if broker_info is None:
+        return None
 
-print(apartment_data)
+    broker_tag = broker_info.select_one("div.listing-card__broker > div.listing-card__broker-info > a")
+    if broker_tag and 'data-broker-agency-name' in broker_tag.attrs:
+        print(f"Processing listing ID {listing_id}")
+        return broker_tag['data-broker-agency-name']
+    return None
+
+def main():
+    page_num = 1
+    all_apartments_data = {}
+
+    while True:
+        soup = get_page_soup(page_num)
+        if soup is None:
+            print("Status_code 404 - No more pages to scrape.")
+            break
+
+        print(f"Scraping page {page_num}...")
+        listings_elements = soup.select("li.normal-results__hit")
+        
+        for listing in listings_elements:
+            listing_details = get_listing_details(listing)
+            listing_id = eval(listing.attrs.get("data-tracking-data", {})).get("listingId", None)
+            all_apartments_data[listing_id] = listing_details
+
+        page_num += 1
+
+    listings_df = pd.DataFrame.from_dict(all_apartments_data, orient="index")
+    
+    print('grabbing broker company names')
+    listings_df['Broker Name'] = listings_df.index.map(lambda x: get_broker_company_name(x))
+    print(listings_df)
+    return listings_df
+
+if __name__ == "__main__":
+   x = main()
+   x.to_excel('hemnet.xlsx')
